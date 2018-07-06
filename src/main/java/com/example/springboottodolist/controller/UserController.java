109f7d133776fa601e4da6847bff41b6c68a2d09
Fedomn <shadowforfiend@gmail.com>
2018-07-06 17:41:15 +0800

    modify user controller

--- src/main/java/com/example/springboottodolist/controller/UserController.java
+++ src/main/java/com/example/springboottodolist/controller/UserController.java
@@ -3,7 +3,6 @@ package com.example.springboottodolist.controller;
 import com.example.springboottodolist.domain.Response;
 import com.example.springboottodolist.domain.Todo;
 import com.example.springboottodolist.domain.User;
-import com.example.springboottodolist.exception.TodoNotFoundException;
 import com.example.springboottodolist.service.TodoService;
 import com.example.springboottodolist.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -27,99 +25,60 @@ public class UserController {
         this.todoService = todoService;
     }
 
-    private ResponseEntity<Response> makeUserNotFoundResponse() {
-        Response<User> response = new Response<>(null, "user not found", HttpStatus.NOT_FOUND);
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
-
-    private ResponseEntity<Response> makeUserResponse(User user) {
-        Response<User> response = new Response<>(user, HttpStatus.OK);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    private ResponseEntity<Response> makeUserResponse(User user, HttpStatus status) {
-        Response<User> response = new Response<>(user, status);
-        return new ResponseEntity<>(response, status);
-    }
-
     @GetMapping("/users")
-    public Response<List<User>> getUsers() {
+    public ResponseEntity<Response> getUsers() {
         List<User> users = userService.findAll();
-        return new Response<>(users, HttpStatus.OK);
+        return new ResponseEntity<>(new Response<>(users), HttpStatus.OK);
     }
 
     @GetMapping("/users/{userId}")
-    public ResponseEntity<Response> getUserById(@PathVariable long userId) {
-        userService.validateUser(userId);
-        Optional<User> user = userService.findById(userId);
-        return user.map(this::makeUserResponse).orElseGet(this::makeUserNotFoundResponse);
+    public ResponseEntity<Response> getUserById(@PathVariable Long userId) {
+        User findUser = userService.findById(userId);
+        return new ResponseEntity<>(new Response<>(findUser), HttpStatus.OK);
     }
 
     @PostMapping("/users")
     private ResponseEntity<Response> saveUser(@RequestBody final User newUser) {
-        User user = userService.save(newUser);
-        return this.makeUserResponse(user, HttpStatus.CREATED);
+        User savedUser = userService.save(newUser);
+        return new ResponseEntity<>(new Response<>(savedUser), HttpStatus.CREATED);
     }
 
     @PutMapping("/users/{userId}")
-    public ResponseEntity<Response> updateById(@PathVariable long userId, @RequestBody final User modifyUser) {
-        Optional<User> user = userService.findById(userId);
-        if (user.isPresent()) {
-            Optional<User> updateUser = userService.updateUserNameById(modifyUser.getName(), userId);
-            return updateUser.map(this::makeUserResponse).orElseGet(this::makeUserNotFoundResponse);
-        } else {
-            return this.makeUserNotFoundResponse();
-        }
+    public ResponseEntity<Response> updateById(@PathVariable Long userId, @RequestBody final User modifyUser) {
+        User findUser = userService.findById(userId);
+        findUser.setName(modifyUser.getName());
+        User newSavedUser = userService.save(findUser);
+        return new ResponseEntity<>(new Response<>(newSavedUser), HttpStatus.OK);
     }
 
     @DeleteMapping("/users/{userId}")
-    public ResponseEntity deleteById(@PathVariable long userId) {
+    public ResponseEntity deleteById(@PathVariable Long userId) {
         userService.deleteById(userId);
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    private ResponseEntity<Response> makeTodosResponse(List<Todo> todos, HttpStatus status) {
-        Response<List<Todo>> response = new Response<>(todos, status);
-        return new ResponseEntity<>(response, status);
-    }
-
-
     @GetMapping("/users/{userId}/todos")
-    public ResponseEntity<Response> getUserTodos(@PathVariable long userId) {
-        userService.validateUser(userId);
-        Optional<User> user = userService.findById(userId);
-        if (user.isPresent()) {
-            List<Todo> todos = user.get().getTodos();
-            return makeTodosResponse(todos, HttpStatus.OK);
-        } else {
-            return makeUserNotFoundResponse();
-        }
+    public ResponseEntity<Response> getUserTodos(@PathVariable Long userId) {
+        User findUser = userService.findById(userId);
+        List<Todo> todos = findUser.getTodos();
+        return new ResponseEntity<>(new Response<>(todos), HttpStatus.OK);
     }
 
     @PostMapping("/users/{userId}/todos")
-    public ResponseEntity<Response> saveUserTodos(@PathVariable long userId, @RequestBody final Todo savedTodo) {
-        userService.validateUser(userId);
-        Optional<User> user = userService.findById(userId);
-        if (user.isPresent()) {
-            User userEntity = user.get();
-            savedTodo.setUser(userEntity);
-
-            userEntity.getTodos().add(savedTodo);
-            userService.save(userEntity);
-            return makeTodosResponse(userEntity.getTodos(), HttpStatus.CREATED);
-        } else {
-            return makeUserNotFoundResponse();
-        }
+    public ResponseEntity<Response> saveUserTodos(@PathVariable Long userId, @RequestBody final Todo savedTodo) {
+        User findUser = userService.findById(userId);
+        savedTodo.setUser(findUser);
+        todoService.save(savedTodo);
+        return new ResponseEntity<>(new Response<>(savedTodo), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/users/{userId}/todos/{todoId}")
-    public ResponseEntity deleteById(@PathVariable long userId, @PathVariable long todoId) {
-        userService.validateUser(userId);
-
-        return todoService.findById(todoId).map(todo -> {
-            todoService.delete(todo);
-            return ResponseEntity.ok().build();
-        }).orElseThrow(() -> new TodoNotFoundException(todoId));
+    public ResponseEntity deleteById(@PathVariable Long userId, @PathVariable Long todoId) {
+        User findUser = userService.findById(userId);
+        Todo findTodo = todoService.findById(todoId);
+        findUser.getTodos().remove(findTodo);
+        userService.save(findUser);
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
