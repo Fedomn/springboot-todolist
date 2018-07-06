811fe7a882f944f4c27775b4a7b1c3abd27aa9cc
Fedomn <shadowforfiend@gmail.com>
2018-07-06 15:08:01 +0800

    add customer exception

--- src/main/java/com/example/springboottodolist/exception/TodoNotFoundException.java
+++ src/main/java/com/example/springboottodolist/exception/TodoNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class TodoNotFoundException extends RuntimeException {
-    public TodoNotFoundException(long id) {
+    public TodoNotFoundException(Long id) {
         super("could not found todo " + id);
     }
 }
